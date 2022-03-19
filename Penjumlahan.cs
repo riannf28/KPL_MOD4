f0662e91c6b99cf1c5e52fed1fff9ad749f7423b
riannf28 <riannurfauzi@student.telkomuniversity.ac.id>
2022-03-19 17:12:30 +0700

    menambahkan class SimpleDataBase

---
+++ Penjumlahan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace modul4_1302200040
+{
+    class Penjumlahan<T>
+    {
+        public void JumlahTigaAngka<T>(ref T a, ref T b, ref T c)
+        {
+            T temp;
+            temp = a;
+            a = b;
+            b = c;
+            c = temp;
+        }
+    }
+}
