504907952dbb6e1286f9377058edc3908d4123d7
riannf28 <riannurfauzi@student.telkomuniversity.ac.id>
2022-03-19 17:20:06 +0700

    implementasi-generic-class

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace modul4_1302200040
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            long a, b, c;
+            a = 13;
+            b = 02;
+            c = 20;
+
+            Console.WriteLine("=====Penjumlahan=====");
+            Penjumlahan<long> jumlah = new Penjumlahan<long>();
+            jumlah.JumlahTigaAngka<long>(ref a, ref b, ref c);
+            Console.WriteLine(a + b + c);
+
+            Console.WriteLine("=====Simple Database=====");
+            SimpleDataBase<int> data = new SimpleDataBase<int>();
+            data.PrintAllData();
+
         }
     }
 }
---
+++ SimpleDataBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace modul4_1302200040
+{
+    class SimpleDataBase<T>
+    {
+        List<T> storedData = new List<T>();
+        List<DateTime> inputDates = new List<DateTime>();
+
+        public void AddNewData<T>(ref T add)
+        {
+
+        }
+
+        public void PrintAllData()
+        {
+            Console.WriteLine("Data 1 berisi: " + storedData + ", yang disimpan pada waktu" + inputDates);
+            Console.WriteLine("Data 2 berisi: " + storedData + ", yang disimpan pada waktu" + inputDates);
+            Console.WriteLine("Data 3 berisi: " + storedData + ", yang disimpan pada waktu" + inputDates);
+        }
+    }
+}
