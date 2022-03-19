e261067c160acbdd79e3ea8e002b74cc6c734ea7
riannf28 <riannurfauzi@student.telkomuniversity.ac.id>
2022-03-19 16:05:37 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace modul4_1302200040
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            long a, b, c;
+            a = 13;
+            b = 02;
+            c = 20;
+            
+            Penjumlahan jumlah = new Penjumlahan();
+            jumlah.JumlahTigaAngka<long>(ref a, ref b, ref c);
         }
     }
 }
